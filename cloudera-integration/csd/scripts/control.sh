be5bad6390519619f514ff2a98609957fb319c45
Naman Jain <namanjain@ip-172-27-239-18.ec2.internal>
2018-08-10 21:53:22 +0530

    Aug 10 9:53pm control.sh update

--- cloudera-integration/csd/scripts/control.sh
+++ cloudera-integration/csd/scripts/control.sh
@@ -40,18 +40,18 @@ export LOGBACK_CONFIG=${LOGBACK_CONFIG:-$DEFAULT_LOGBACK_CONFIG}
 export KEYTAB_FILE=${KEYTAB_FILE:-"${CONF_DIR}/pulse.keytab"}
 export AKKA_CONF=${AKKA_CONF:-"application.conf"}
 
-if [[ ( -f COLLECTION_ROLLER_CONFIG ) && ( ! -s COLLECTION_ROLLER_CONFIG ) ]]; then #Checking if file exists AND it's non-empty
-    COLLECTION_ROLLER_CONFIG=${DEFAULT_COLLECTION_ROLLER_CONFIG}
+if [[ ( -f "$COLLECTION_ROLLER_CONFIG" ) && ( ! -s "$COLLECTION_ROLLER_CONFIG" ) ]]; then #Checking if file exists AND it's non-empty
+    COLLECTION_ROLLER_CONFIG=${COLLECTION_ROLLER_CONFIG}
 else
     DEFAULT_COLLECTION_ROLLER_CONFIG=$(readlink -f cloudera-integration/csd/aux/default-collection-roller.yml)
-    COLLECTION_ROLLER_CONFIG=${COLLECTION_ROLLER_CONFIG}
+    COLLECTION_ROLLER_CONFIG=${DEFAULT_COLLECTION_ROLLER_CONFIG}
 fi
 
-if [[ ( -f ALERT_ENGINE_CONFIG ) && ( ! -s ALERT_ENGINE_CONFIG ) ]]; then #Checking if file exists AND it's non-empty
-    ALERT_ENGINE_CONFIG=${DEFAULT_ALERT_ENGINE_CONFIG}
+if [[ ( -f "$ALERT_ENGINE_CONFIG" ) && ( ! -s "$ALERT_ENGINE_CONFIG" ) ]]; then #Checking if file exists AND it's non-empty
+    ALERT_ENGINE_CONFIG=${ALERT_ENGINE_CONFIG}
 else
     DEFAULT_ALERT_ENGINE_CONFIG=$(readlink -f cloudera-integration/csd/aux/default-alert-engine.yml)
-    ALERT_ENGINE_CONFIG=${ALERT_ENGINE_CONFIG}
+    ALERT_ENGINE_CONFIG=${DEFAULT_ALERT_ENGINE_CONFIG}
 fi
 
 export JAAS_CONFIG="Client {
