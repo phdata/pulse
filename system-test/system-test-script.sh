f558062ab4c60d931555f61f5540acb66ddaf923
Shashi Reddy <sreddy@phdata.io>
2018-08-20 11:48:33 -0500

    System test changes made

--- system-test/system-test-script.sh
+++ system-test/system-test-script.sh
@@ -1,80 +1,28 @@
-# !/usr/bin/env bash
-# Stopping the script if any command fails
-set -euo pipefail
+#!/usr/bin/env bash
+set -e
+cd ..
 
-export collection_roller_log="system-test/collection-roller_logfile"
-export alert_engine_log="system-test/alert-engine_logfile"
-export log_collector_log="system-test/log-collector_logfile"
-export spark_log="system-test/spark_logfile"
+echo "starting collection roller"
+bin/collection-roller &> /dev/null &
 
-echo "Starting collection roller....."
-bin/collection-roller 2>&1 > $collection_roller_log &
-# Getting the process id of collection roller
-collection_roller_pid=$!
-# Checking if the collection roller service has started
-if [ -z "$collection_roller_pid" ]
-then
-      echo "Collection Roller is not running"
-      exit 1
+echo "starting alert engine"
+bin/alert-engine &> /dev/null &
+
+echo "starting log collector"
+bin/log-collector &> /dev/null &
+
+echo "starting spark application"
+cd /home/username/pulse/log-example/
+./spark-logging &> /dev/null &
+sleep 10 &
+echo "Curing the Solr API"
+var10=$(curl -su 'username' 'http://zk.host:8983/solr/logging-pulse_latest_shard1_replica1/select?q=*%3A*&wt=json&indent=true')
+
+if [[ "$var10" =~ "\"numFound\":0" ]]; then
+        echo "No Documents"
+        exit 1
 else
-      echo "Collection Roller is running....."
-      #exit 0
+        echo "Documents collected"
+        exit 0
 fi
-echo "Starting alert engine....."
-bin/alert-engine 2>&1 > $alert_engine_log &
-# Getting process of alert engine
-alert_engine_pid=$!
-# Checking if the alert engine service has started
-if [ -z "$alert_engine_pid" ]
-then
-      echo "Alert Engine is not running"
-      exit 1
-else
-      echo "Alert Engine is running...."
-      #exit 0
-fi
-echo "Starting log collector...."
-bin/log-collector 2>&1 > $log_collector_log &
-# Getting process of log collector
-log_collector_pid=$!
-# Checking if the log collector service has started
-if [ -z "log_collector_pid" ]
-then
-      echo "Log collector is not running"
-      exit 1
-else
-      echo "Log Collector is running...."
-      #exit 0
-fi
-
-cd /home/mgeorge/pulse/log-example/
-./spark-logging 2>&1 > $spark_log &
-
-echo "Curling the Solr API"
-# Asking for edge node credentials
-read -p "Username: " USER
-query_response=$(curl -i -X GET \
-                   'http://master3.valhalla.phdata.io:8983/solr/logging-pulse_latest/select?q=%2A%3A%2A&wt=json&indent=true' \
-                   -H 'Authorization: Basic bWdlb3JnZTp3czJlZDNyZjQh' \
-                   -H 'Cache-Control: no-cache' \
-                   -H 'Postman-Token: cc1420a0-9efc-4227-9cb8-be129a9ebd94')
-http_status_collection=$(echo "$query_response" | grep HTTP |  awk '{print $2}')
-echo $http_status_collection
-# Checking if the collection exists and if documents are collected
-if [[  "$http_status_collection" == 200 ]]; then
-       #echo "Passed"
-       if [[ "query_response" =~ "\"numFound\":0" ]]; then
-                echo "Failed!"
-       else
-                echo "Passed!"
-       fi
-else
-        echo "Failed"
-fi
-
-# Killing service PIDs
-echo "Killing service PIDS"
-kill -9 $log_collector_pid
-kill -9 $collection_roller_pid
-kill -9 $alert_engine_pid
 
