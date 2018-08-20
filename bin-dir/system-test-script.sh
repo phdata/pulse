52669f888d9cb549735b8a9328ed5bd9f609f3aa
Shashi Reddy <sreddy@phdata.io>
2018-08-20 10:58:00 -0500

    groupTriggeredAlerts case classes are built directly instead of parsing yaml file into case classes.

--- bin-dir/system-test-script.sh
+++ bin-dir/system-test-script.sh
@@ -12,11 +12,11 @@ echo "starting log collector"
 bin/log-collector &> /dev/null &
 
 echo "starting spark application"
-cd /home/sreddy/pulse/log-example/
+cd /home/username/pulse/log-example/
 ./spark-logging &> /dev/null &
 sleep 10 &
 echo "Curing the Solr API"
-var10=$(curl -su 'sreddy' 'http://worker1.valhalla.phdata.io:8983/solr/logging-pulse_1534384327_shard1_replica1/select?q=*%3A*&wt=json&indent=true')
+var10=$(curl -su 'username' 'http://zk.host:8983/solr/logging-pulse_latest_shard1_replica1/select?q=*%3A*&wt=json&indent=true')
 
 if [[ "$var10" =~ "\"numFound\":0" ]]; then
         echo "No Documents"
