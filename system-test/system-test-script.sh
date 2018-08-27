056f46f651ce7803ed0afe03f0bc3392fc251554
Shashi Reddy <sreddy@phdata.io>
2018-08-27 16:21:45 -0500

    Added trap functionality to script

--- system-test/system-test-script.sh
+++ system-test/system-test-script.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 # Stopping the script if any command fails
-#set -euo pipefail
 set -e
+source bin/env.sh
 function cleanup {
-  echo "Removing /tmp/foo"
-  rm  -r /tmp/system-check
+  echo "Removing /tmp/pulse-system-test"
+  rm  -r /tmp/pulse-system-test
 }
 trap cleanup EXIT
-mkdir /tmp/system-check
+mkdir /tmp/pulse-system-test
 
 export collection_roller_log="system-test/log_files/collectionroller.log"
 export alert_engine_log="system-test/log_files/alertengine.log"
 export log_collector_log="system-test/log_files/logcollector.log"
 
 echo "Starting collection roller....."
-bin/collection-roller 2>&1 > $collection_roller_log
+bin/collection-roller 2>&1 > $collection_roller_log &
 # Getting the process id of collection roller
 collection_roller_pid=$!
 # Checking if the collection roller service has started
 if [ -z "$collection_roller_pid" ]
 then
       echo "Collection Roller is not running"
-      killing
       exit 1
 fi
 echo "Starting alert engine....."
@@ -31,19 +30,17 @@ alert_engine_pid=$!
 # Checking if the alert engine service has started
 if [ -z "$alert_engine_pid" ]
 then
-      echo "Alert Engine is not running"s
-      killing
+      echo "Alert Engine is not running"
       exit 1
 fi
 echo "Starting log collector...."
-bin/log-collector 2>&1 > $log_collector_log
+bin/log-collector 2>&1 > $log_collector_log &
 # Getting process of log collector
 log_collector_pid=$!
 # Checking if the log collector service has started
 if [ -z "log_collector_pid" ]
 then
       echo "Log collector is not running"
-      killing
       exit 1
 fi
 
@@ -52,7 +49,7 @@ fi
 echo "Curling the Solr API"
 # Asking for edge node credentials
 read -p "Username: " USER
-query_response=$(curl -i -o - --silent -X GET -u $USER "http://master3.valhalla.phdata.io:8983/solr/logging-pulse_latest/select?q=*%3A*&wt=json&indent=true")
+query_response=$(curl -i -o - --silent -X GET -u ${SOLR_USR}:${SOLR_PWD} "http://master3.valhalla.phdata.io:8983/solr/logging-pulse-test_latest/select?q=*%3A*&wt=json&indent=true")
 http_status_collection=$(echo "$query_response" | grep HTTP |  awk '{print $2}')
 echo $http_status_collection
 # Checking if the collection exists and if documents are collected
@@ -65,4 +62,3 @@ if [[  "$http_status_collection" == 200 ]]; then
 else
         echo "Failed"
 fi
-
