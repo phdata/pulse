ae023311aa1e2482bd5fabaca159ed46e6ef9860
shashireddy <19143691+shashi4569@users.noreply.github.com>
2018-09-01 03:01:46 -0500

    Combination of 7 commits
    Updated script with while and kill services.
    
    While loop to check port availability and killing parent group ID to kill all child programs(PID's).
    
    comment fix
    
    Fixed the SMTP password configuration regression. (#51)
    
    Not setting a SMTP password in CM will cause the alert engine to skip SMTP auth.
    
    version bump.
    
    Shell script upgraded while loop to check port availability before running any sample application for pulse. And also added
    
    Shell script upgraded while loop to check port availability before running any sample application for pulse. And also added
    
    Shell script upgraded while loop to check port availability before running any sample application for pulse. And also added

--- log-example/src/main/scala/io/phdata/pulse/example/SparkLog4jExample.scala
+++ log-example/src/main/scala/io/phdata/pulse/example/SparkLog4jExample.scala
@@ -59,15 +59,15 @@ object SparkLog4jExample {
     }
 
     testRdd.foreach { num =>
-        LoggingConfiguration.init // initialized once on first record, value is thrown away an nothing done for other records
-        if (num % 1000 == 0) {
-          log.error(s"XXXXX error! num: " + num)
-        } else if (num % 100 == 0) {
-          log.warn(s"XXXXX warning! num: " + num)
-        } else {
-          log.info(s"XXXXX found: " + num)
-        }
+      LoggingConfiguration.init // initialized once on first record, value is thrown away an nothing done for other records
+      if (num % 10000 == 0) {
+        log.error(s"XXXXX error! num: " + num)
+      } else if (num % 5000 == 0) {
+        log.warn(s"XXXXX warning! num: " + num)
+      } else {
+        log.info(s"XXXXX found: " + num)
       }
+    }
 
     log.info("Shutting down the spark logging example")
     sc.stop()
