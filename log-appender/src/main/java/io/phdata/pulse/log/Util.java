1970bef09fd5858805241a7df604b529f1abd01a
Tony Foerster <tony@phdata.io>
2018-12-11 11:27:22 -0600

    Log spark application id and container id
    The log appender will check if the application is Spark with a System
    variable then append the container and application id.

--- log-appender/src/main/java/io/phdata/pulse/log/Util.java
+++ log-appender/src/main/java/io/phdata/pulse/log/Util.java
@@ -1,5 +1,8 @@
 package io.phdata.pulse.log;
 
+
+import org.apache.log4j.helpers.LogLog;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,6 +10,8 @@ import java.util.TimeZone;
 
 public class Util {
   public static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+  public static final String YARN_LOG_DIR_SYSTEM_PROPERTY = "spark.yarn.app.container.log.dir";
+  public static final String YARN_APP_ID_PROPERTY = "spark.yarn.app.id";
 
   public static String epochTimestampToISO8061(long ts) {
     Date date = new Date(ts);
@@ -14,5 +19,39 @@ public class Util {
     // @TODO get timezone from environment, this will use UTC always
     dateFormat.setTimeZone(UTC);
     return dateFormat.format(date);
+  }
+
+  public static boolean isSparkApplication() {
+    return System.getProperty(YARN_LOG_DIR_SYSTEM_PROPERTY) != null;
+  }
+
+  public static String getApplicationId() {
+    try {
+      String[] segments = System.getProperty(YARN_LOG_DIR_SYSTEM_PROPERTY).split("/");
+      if (segments.length == 0)
+        return System.getProperty(YARN_APP_ID_PROPERTY);
+      else
+        return segments[segments.length - 2];
+    } catch (Exception e) {
+      LogLog.error("Could not set Spark application id: " + e, e);
+      return "";
+    }
+  }
+
+  public static String getContainerId() {
+    try {
+      String[] segments = System.getProperty(YARN_LOG_DIR_SYSTEM_PROPERTY).split("/");
+      if (segments.length == 0)
+        return "Driver";
+      else {
+        String[] underSplit = segments[segments.length - 1].split("_");
+        // concatenate the container attempt and container number
+        String value = underSplit[underSplit.length - 2] + "_" + underSplit[underSplit.length - 1];
+        return value;
+      }
+    } catch (Exception e) {
+      LogLog.error("Could not set Spark container id: " + e, e);
+      return "";
+    }
   }
 }
