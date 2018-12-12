8b9ad6069619b5d3dd9abd835c44ebd9f04be15d
sada3390 <40669724+sada3390@users.noreply.github.com>
2018-12-12 11:28:15 -0600

    updated json and bash replace script (#70)

--- visualization/arcadia/replace_dataset_name.sh
+++ visualization/arcadia/replace_dataset_name.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
-# Usage: ./replace_dataset_name.sh <json_file_to_replace_from> <your_dataset_name>
-# This script will replace value of dataset_name from the json file provided
-
+# Usage: ./replace_dataset_name.sh <json_file_to_replace_from> <new_dataset_name> <new_dataset_detail>
+# This script will replace value of dataset_name, dataset_detail from pulse.json file
 
 JSON_FILE=$1
 NEW_DATASET_NAME=$2
+NEW_DATASET_DETAIL=$3
 
-sed -i "" "s/\"dataset_name\": \"[^\"]*\"/\"dataset_name\": \"$NEW_DATASET_NAME\"/" $JSON_FILE
+sed -i "" "s/pulse-test-temp/$NEW_DATASET_NAME/g; s/Solr.pulse-test-default_all/$NEW_DATASET_DETAIL/g" $JSON_FILE
