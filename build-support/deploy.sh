78f0645051d7da574604f2db0c1e07a4804d3de4
Tony Foerster <tony@phdata.io>
2018-12-20 11:33:25 -0600

    Update deploy
    Include 'CDH' in repo name

--- build-support/deploy.sh
+++ build-support/deploy.sh
@@ -7,14 +7,14 @@ projectVersion=$(make --no-print-directory --silent version)
 repoName=${projectVersion}-cdh5
 parcelVersion=$(make --no-print-directory --silent -C cloudera-integration/parcel version)
 
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/manifest.json "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/manifest.json"
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/PULSE-${parcelVersion}-el7.parcel"
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/PULSE-${parcelVersion}-el7.parcel.sha"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/manifest.json "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/manifest.json"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-el7.parcel"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-el7.parcel.sha"
 
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/PULSE-${parcelVersion}-el6.parcel"
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/PULSE-${parcelVersion}-el6.parcel.sha"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-el6.parcel"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-el6.parcel.sha"
 
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}.jar "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/PULSE-${parcelVersion}.jar"
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}.jar.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${projectVersion}/PULSE-${parcelVersion}.jar.sha"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}.jar "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}.jar"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}.jar.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}.jar.sha"
 
 echo deploy success
