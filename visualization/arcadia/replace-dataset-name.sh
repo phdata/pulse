956acbbf08853a9fda69956b2388cfcbf47603f2
Tony Foerster <tony@phdata.io>
2019-01-03 14:53:16 -0600

    Move Arcadia docs into mkdocks

---
+++ visualization/arcadia/replace-dataset-name.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+set -xe
+# Usage: ./replace_dataset_name.sh <template> <dataset_name> <dataset_detail>
+# This script will replace value of dataset_name, dataset_detail from pulse.json file
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage './replace-data-set-name.sh <dataset_name> <dataset_source>"
+    exit 1
+fi
+
+NEW_DATASET_NAME=$2
+DATASET_SOURCE=$3
+
+sed "s/pulse-test-temp/$NEW_DATASET_NAME/g; s/Solr.pulse-test-default_all/$DATASET_SOURCE/g" pulse-dashboard-template.json > dashboard.json
