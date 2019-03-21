a15f68b1d8e2d5c71af7747fd8e25b3b6d3da7b5
Tony Foerster <tony@phdata.io>
2019-03-21 07:05:33 -0500

    parent bb22ab3da15f1adcc9ebe9d1f3776ad4a224d5a1 author Tony Foerster <tony@phdata.io> 1553169933 -0500 committer safwanislam <safwan.islam10@gmail.com> 1553207354 -0500
    Add new version to install docs
    
    -fixed LogCollectorCliParser for added Kafka arguments
    -added cli parser test cases
    -incorporated Kafka mode for LogCollector
    
    TODO: PulseKafkaConsumer(line 58) - what should behavior be when 'application' has no value

--- log-collector/src/main/scala/io/phdata/pulse/logcollector/LogCollectorCliParser.scala
+++ log-collector/src/main/scala/io/phdata/pulse/logcollector/LogCollectorCliParser.scala
@@ -21,5 +21,22 @@ import org.rogach.scallop.ScallopConf
 class LogCollectorCliParser(args: Seq[String]) extends ScallopConf(args) {
   lazy val port    = opt[Int]("port", required = true, descr = "Listening port")
   lazy val zkHosts = opt[String]("zk-hosts", required = true, descr = "Zookeeper hosts")
+  lazy val topic   = opt[String]("topic", required = false, descr = "Kafka Topic")
+  lazy val mode = opt[String]("consume-mode",
+                              required = false,
+                              descr = "'http' or 'kafka'",
+                              default = Some("http"))
+
+  validateOpt(mode, port) {
+    case (Some("http"), None) => Left("Need a port if running http mode")
+    case (None, None)         => Left("Need a port if running http mode")
+    case _                    => Right(Unit)
+  }
+
+  validateOpt(mode, topic) {
+    case (Some("kafka"), None) => Left("Need a topic if running kafka mode")
+    case _                     => Right(Unit)
+  }
+
   verify()
 }
