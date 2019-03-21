ce43fa8044045f0b5f9b7c0446585507f92740b3
safwanislam <safwan.islam10@gmail.com>
2019-03-21 17:52:23 -0500

    modified kafka consume method to align with new PulseKafkaConsumer class
    added kafka arguments and argument validation
    
    -fixed LogCollectorCliParser for added Kafka arguments
    -added cli parser test cases
    -incorporated Kafka mode for LogCollector
    
    TODO: PulseKafkaConsumer(line 58) - what should behavior be when 'application' has no value
    
    -added kafka-properties argument to cli parser
    -moved serve function outside the main method
    -read kafka consumer properties from new passed in file
    -modified test cases for new argument
    
    -convert log event to Map[String,String] before putting on solr stream
    -add file appender to log4j for log-example
    -adding kafka mode variables to env.sh
    -adding mode controlling in log-collector script
    -created log-stash script to turn on logstash
    -adding Kafka client to jaas.conf and a start kafka mode log collector to control.sh
    -updated logstash.conf file
    
    -changed method names in LogCollector
    -added more descriptive argument validation
    -changed name of output log for StandAloneApp
    -placed "set -eu" in each bin script
    -changed name of output log for StandAloneApp for logstash conf file

--- log-collector/src/main/scala/io/phdata/pulse/logcollector/LogCollector.scala
+++ log-collector/src/main/scala/io/phdata/pulse/logcollector/LogCollector.scala
@@ -16,6 +16,7 @@
 
 package io.phdata.pulse.logcollector
 
+import java.io.FileInputStream
 import java.util.Properties
 import java.util.concurrent.Executors
 
@@ -24,7 +25,6 @@ import akka.http.scaladsl.Http
 import akka.stream.{ ActorMaterializer, Materializer }
 import com.typesafe.scalalogging.LazyLogging
 import io.phdata.pulse.common.SolrService
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.solr.client.solrj.impl.CloudSolrServer
 
 import scala.concurrent.duration.Duration
@@ -48,54 +48,51 @@ object LogCollector extends LazyLogging {
     val solrServer  = new CloudSolrServer(cliParser.zkHosts())
     val solrService = new SolrService(cliParser.zkHosts(), solrServer)
 
-    // Akka System
-    implicit val actorSystem: ActorSystem   = ActorSystem()
-    implicit val materializer: Materializer = ActorMaterializer.create(actorSystem)
-    implicit val ec                         = actorSystem.dispatchers.lookup("akka.actor.http-dispatcher")
-
     val routes = new LogCollectorRoutes(solrService)
-
-    def serve(port: Int): Unit = {
-      // Starts Http Service
-      val httpServerFuture = Http().bindAndHandle(routes.routes, "0.0.0.0", port = port)(
-        materializer) map { binding =>
-        logger.info(s"Log Collector interface bound to: ${binding.localAddress}")
-      }
-
-      httpServerFuture.onComplete {
-        case Success(v) => ()
-        case Failure(e) => throw new RuntimeException(e)
-      }
-
-      // Start LogCollector HttpService
-      Await.ready(
-        httpServerFuture,
-        Duration.Inf
-      )
-    }
 
     cliParser.mode() match {
       case "kafka" => {
-        consume(solrService, cliParser.zkHosts(), cliParser.port(), cliParser.topic())
+        kafka(solrService, cliParser.kafkaProps(), cliParser.topic())
       }
       case _ => {
-        serve(cliParser.port())
+        http(cliParser.port(), routes)
       }
     }
   }
 
+  def http(port: Int, routes: LogCollectorRoutes): Future[Unit] = {
+    // Akka System
+    implicit val actorSystem: ActorSystem   = ActorSystem()
+    implicit val ec                         = actorSystem.dispatchers.lookup("akka.actor.http-dispatcher")
+    implicit val materializer: Materializer = ActorMaterializer.create(actorSystem)
+
+    // Starts Http Service
+    val httpServerFuture = Http().bindAndHandle(routes.routes, "0.0.0.0", port)(materializer) map {
+      binding =>
+        logger.info(s"Log Collector interface bound to: ${binding.localAddress}")
+    }
+
+    httpServerFuture.onComplete {
+      case Success(v) => ()
+      case Failure(e) => throw new RuntimeException(e)
+    }
+
+    // Start LogCollector HttpService
+    Await.ready(
+      httpServerFuture,
+      Duration.Inf
+    )
+  }
+
   // Starts Kafka Consumer
-  def consume(solrService: SolrService, zkHost: String, port: Int, topic: String): Unit = {
-    val kafkaConsumer      = new PulseKafkaConsumer(solrService)
+  def kafka(solrService: SolrService, kafkaProps: String, topic: String): Unit = {
+
+    val solrCloudStream = new SolrCloudStream(solrService)
+
+    val kafkaConsumer      = new PulseKafkaConsumer(solrCloudStream)
     val kafkaConsumerProps = new Properties()
 
-    kafkaConsumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, s"$zkHost:$port")
-    kafkaConsumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                           "org.apache.kafka.common.serialization.StringDeserializer")
-    kafkaConsumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                           "org.apache.kafka.common.serialization.StringDeserializer")
-    kafkaConsumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-    kafkaConsumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "pulse-kafka")
+    kafkaConsumerProps.load(new FileInputStream(kafkaProps))
 
     kafkaConsumer.read(kafkaConsumerProps, topic)
   }
--- log-collector/src/main/scala/io/phdata/pulse/logcollector/LogCollectorCliParser.scala
+++ log-collector/src/main/scala/io/phdata/pulse/logcollector/LogCollectorCliParser.scala
@@ -19,23 +19,28 @@ package io.phdata.pulse.logcollector
 import org.rogach.scallop.ScallopConf
 
 class LogCollectorCliParser(args: Seq[String]) extends ScallopConf(args) {
-  lazy val port    = opt[Int]("port", required = true, descr = "Listening port")
+  lazy val port    = opt[Int]("port", required = false, descr = "HTTP Server Listening port")
   lazy val zkHosts = opt[String]("zk-hosts", required = true, descr = "Zookeeper hosts")
-  lazy val topic   = opt[String]("topic", required = false, descr = "Kafka Topic")
   lazy val mode = opt[String]("consume-mode",
                               required = false,
                               descr = "'http' or 'kafka'",
                               default = Some("http"))
+  lazy val kafkaProps =
+    opt[String]("kafka-properties", required = false, descr = "Kafka properties file")
+  lazy val topic = opt[String]("topic", required = false, descr = "Kafka Topic")
 
   validateOpt(mode, port) {
-    case (Some("http"), None) => Left("Need a port if running http mode")
-    case (None, None)         => Left("Need a port if running http mode")
-    case _                    => Right(Unit)
+    case (Some("http") | None, None) => Left("Need a port if running http mode")
+    case _                           => Right(Unit)
   }
 
-  validateOpt(mode, topic) {
-    case (Some("kafka"), None) => Left("Need a topic if running kafka mode")
-    case _                     => Right(Unit)
+  validateOpt(mode, kafkaProps, topic) {
+    case (Some("kafka"), None, Some(_)) =>
+      Left("--kafka-properties argument needed if --consume-mode=kafka")
+    case (Some("kafka"), Some(_), None) => Left("--topic argument needed if --consume-mode=kafka")
+    case (Some("kafka"), None, None) =>
+      Left("--topic and --kafka-properties arguments needed if --consume-mode=kafka")
+    case _ => Right(Unit)
   }
 
   verify()
