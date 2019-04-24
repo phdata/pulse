d8c4b9c9983c63b747c940c9f17995375a0b284d
Jordan Birdsell <jbirdsell@phdata.io>
2019-04-24 00:05:24 -0400

    Multiple changes. Most related to logging handler and a few documentation changes.

--- appenders/python/pulse/formatters.py
+++ appenders/python/pulse/formatters.py
@@ -1,35 +1,30 @@
 import logging
 import datetime
-import json
 from pytz import utc
 
 
-class StandardFormatter(logging.Formatter):
+class PulseFormatter(logging.Formatter):
     """
-    A Formatter Class that created a JSON record format for Pulse.
+    A Formatter Class that creates a condensed dictionary for Pulse.
     """
-
-    def __init__(self):
-        """
-        Initializes StandardFormatter
-        """
-        logging.Formatter.__init__(self)
 
     def format(self, record):
         """
-        Covert LogRecord to JSON.
+        Condenses LogRecord to smaller Dictionary.
 
         :param record: :class:`LogRecord` to format
         :type record: logging.LogRecord
-        :returns: JSON formatted string
-        :rtype: str
+        :returns: Condensed dictionary
+        :rtype: dict
         """
         data = dict()
 
         data["category"] = record.name
-        data["timestamp"] = datetime.datetime.utcnow().replace(tzinfo=utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+        data["timestamp"] = datetime.datetime.utcnow()\
+            .replace(tzinfo=utc)\
+            .strftime('%Y-%m-%dT%H:%M:%SZ')
         data["level"] = record.levelname
         data["message"] = record.msg
         data["threadName"] = record.threadName
 
-        return json.dumps(data)
+        return data
