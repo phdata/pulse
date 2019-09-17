2e794e3eb417709117a81ee6c27dbaa53cccaf45
Tony Foerster <tony@phdata.io>
2019-09-17 08:49:11 -0500

    Fix path

--- build-support/deploy.sh
+++ build-support/deploy.sh
@@ -9,14 +9,17 @@ repoName=${projectVersion}-${cdh_version}
 parcelVersion=$(make --no-print-directory --silent -C cloudera-integration/parcel version)
 dist=dist$CDH_VERSION
 
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/manifest.json "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/manifest.json"
+pwd
+ls
+
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T $dist/manifest.json "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/manifest.json"
 
 for distro in "el6" "el7" "sles12"; do
-  curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-$distro.parcel"
-  curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}-el7.parcel.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-$distro.parcel.sha"
+  curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T $dist/PULSE-${parcelVersion}-el7.parcel "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-$distro.parcel"
+  curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T $dist/PULSE-${parcelVersion}-el7.parcel.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}-$distro.parcel.sha"
 done
 
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}.jar "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}.jar"
-curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T dist/PULSE-${parcelVersion}.jar.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}.jar.sha"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T $dist/PULSE-${parcelVersion}.jar "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}.jar"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -T $dist/PULSE-${parcelVersion}.jar.sha "https://repository.phdata.io/artifactory/$DEPLOY_REPO/phdata/pulse/${repoName}/PULSE-${parcelVersion}.jar.sha"
 
 echo deploy success
